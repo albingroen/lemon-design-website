a82e2258f1239e3ea63d743febc4f0d41592cd7d
Albin Groen <albin.groen@gmail.com>
2022-04-11 21:40:04 +0200

    Add splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "lemon-design-react/dist/lemon.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import splitbee from "@splitbee/web";
+
+splitbee.init();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
